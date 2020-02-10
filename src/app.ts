ed53facb8c54af514f0ea9a687cb5cda02ae347f
ms2579 <adnansumra115@gmail.com>
2020-02-10 00:07:11 -0500

    Configure Express & TS

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+
+const app: express.Application = express(); 
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello');
+}); 
+
+app.listen(5000, () => {
+  console.log('Server Start')
+});
