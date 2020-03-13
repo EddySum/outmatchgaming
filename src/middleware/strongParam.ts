84dcca7d3e409a7f3c43fcf9e80f1591ddd2deb1
Eddy Sumra <adnansumra115@gmail.com>
2020-03-13 02:09:31 -0400

    strong param middleware

---
+++ src/middleware/strongParam.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express"
+
+// enter expected object data for params
+export const strongParam = (params: {[key:string]: string}) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const weakParams = req.body;
+    req.body = null; //ensure weak params are not used
+
+    Object.entries(params).forEach(([key, expectedType]) => {
+    const weakParamType = typeof weakParams[key];
+    console.log(weakParamType, expectedType);
+    if (weakParamType === expectedType) {
+      res.locals[key] = weakParams[key];
+    }
+
+    });
+    next();
+  } 
+}
